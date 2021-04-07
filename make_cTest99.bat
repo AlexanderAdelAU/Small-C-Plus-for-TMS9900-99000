94cc11f87141934f328f9c5fc111a08b4b63aade
adel314 <38694473+adel314@users.noreply.github.com>
2021-04-07 21:07:05 +0930

    Add files via upload

---
+++ make_cTest99.bat
@@ -0,0 +1,17 @@
+REM Clear Read-Only Attributes
+attrib  -r *.COM
+attrib  -r *.L99
+attrib  -r *.R$
+attrib  -r *.D$
+REM Delete the old files
+del *.COM
+del *.R$
+del *.D$
+del *.L99
+smallcp -C cTest99
+smallcp -C fptable
+R99 cTest99 SCHCLC
+link99  -M  -S cTest99 clib99.LIB iolib99.LIB strlib99.LIB fplib99.LIB
+REM link99 -M cTest99 abc
+REM link99 -M cFTest cfloat
+REM  drel cTest99.R99
