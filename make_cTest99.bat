de7a1a7aef9061446c1b00f2deeb4e7f1a3cdd4b
adel314 <38694473+adel314@users.noreply.github.com>
2021-04-07 21:07:40 +0930

    Update make_cTest99.bat

--- make_cTest99.bat
+++ make_cTest99.bat
@@ -12,6 +12,4 @@ smallcp -C cTest99
 smallcp -C fptable
 R99 cTest99 SCHCLC
 link99  -M  -S cTest99 clib99.LIB iolib99.LIB strlib99.LIB fplib99.LIB
-REM link99 -M cTest99 abc
-REM link99 -M cFTest cfloat
-REM  drel cTest99.R99
+
