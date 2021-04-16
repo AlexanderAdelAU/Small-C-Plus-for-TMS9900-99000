719cf5504699b5f066dbd43c363d0495ee71343a
adel314 <38694473+adel314@users.noreply.github.com>
2021-04-16 12:06:58 +0930

    Add files via upload

--- iolib/float.h
+++ iolib/float.h
@@ -1,32 +1,11 @@
-/*extern double _fpexp();
-extern double _fplog();
-extern double _fpsqrt();
-extern double _fppow();
-extern double _fpsin();
-extern double _fpcos();
-extern double _fptan();
-
-*/
-/*
- *	fp floor function
- */
-
-extern double _ccfloor();
 extern double floor();
-/*floor ()
+extern double float();
+extern int ifix();
 
-{
-	return _ccfloor();
-}
-*/
-double _ccfloat();
-double float();
-/*float ()
-
-{
-	return _ccfloat();
-}
-*/
-extern ifix();
-double _ccifix();
-
+extern double sin();
+extern double cos();
+extern double tan();
+extern double pow();
+extern double fabs();
+extern double log();
+extern double exp();
