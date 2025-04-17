e1d64b7fdc7a344252bec59b6cc29089343190d2
Alex <38694473+AlexanderAdelAU@users.noreply.github.com>
2025-04-17 17:26:45 +1000

    Add files via upload

--- fplib99/MAKE_FP99.bat
+++ fplib99/MAKE_FP99.bat
@@ -14,18 +14,16 @@ del *.L$
 del *.N$
 
 REM================================
-REM Floating Point Library
+REM Floating Point Library Interface
 REM ===============================
 smallcp -C -M float\float
 R99 float/float SCHCLC
 copy float\*.R99
 R99 cfloatm48 SCHCLC
 
-
-
 REM Make the library
 lib99 -U fplib99 float cfloatM48
-rem del *.R99
+
 
 
 
